3545f402677d15ee7c25373f23f64d43a4438efe
hernanx32 <webfsa@gmail.com>
2026-07-17 19:02:12 -0300

    adasdasd

--- index.php
+++ index.php
@@ -7,72 +7,35 @@ $mensaje='';
 include("mod/basico.php");
 include("mod/conex.php");
 
-
 cabeza($titulo,$path);
-	
 
-
-?>
-
- 
-    
-<body>
-
-<body class="hold-transition login-page">
-<div class="login-box">
-  <!-- /.login-logo -->
-  <div class="card card-outline card-primary">
-    <div class="card-header text-center">
-      <a class="h1"><b>Sistema de</b> Gestión</a>
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg"><?php echo $mensaje; ?></p>
-
-      <form id="form1" name="form1"  action="index.php?scr=ingresar" method="post">
-        <div class="input-group mb-3">
-          <input name="usuario" type="text" required="required" class="form-control" id="usuario" placeholder="Usuario" onkeypress="return bajarEnter(this, event)" size="20" maxlength="20">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input name="clave" type="password" required="required" class="form-control" id="clave" placeholder="Contraseña" size="20" maxlength="20">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-  <div class="row">
-          <!-- /.col -->
-    <div class="col-5">
-    <a href="index.php?scr=olvidoclave">¿Olvido Su Clave?</a>
-   </div>         
-    <div class="col-7">
-    <button type="submit" class="btn btn-primary btn-block">Ingresar</button>
-    </div>
-
-          <!-- /.col -->
-        </div>
-      </form>
- 
+  
+if (isset($_GET['msj']))
+{
+    $mensaje="<strong><span style='color: red;'>¡El Usuario y Clave son Incorrectos!..</span></strong>";    
    
-    </div>
-    <!-- /.card-body -->
-  </div>
-  <!-- /.card -->
-</div>
-	
-	
-	
-	
+} else {
+    $mensaje="<strong><span style='color: Green;'>Bienvenido..</span></strong>";  
+}
 
+if (!isset($_GET['scr'])){
+    
+    include("mod/login/login.php");
+    $focus='usuario';
+}else{
+    $scr=$_GET['scr'];
+
+    if ($scr=="ingresar"){
+        include("mod/login/ingresar.php");
+        $focus='usuario';   
+    }
+    if($scr=="olvidoclave"){
+    include("mod/login/olvidoclave.php");
+    $focus='correo';
+    }
+}
 	
 	
-	
-	<?PHP 
 	
 	echo "<BR>";
 	echo "Servidor:".$config['servidor']['ip']; 
